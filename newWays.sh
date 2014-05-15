40df09de9b32608205abde1fe4b2d63c7159f327
andrewdefries <andrew.defries@gmail.com>
2014-05-15 03:27:14 +0000

    11 workers with worklists

--- newWays.sh
+++ newWays.sh
@@ -3,12 +3,24 @@ rm RunLog
 
 #####
 #R CMD BATCH CreatNewTable.R
+gsutil ls gs://pubchem/*.sdf | sed -n '1,235p' > WorkList0
+gsutil ls gs://pubchem/*.sdf | sed -n '236,470p' > WorkList1
+gsutil ls gs://pubchem/*.sdf | sed -n '471,705p' > WorkList2
+gsutil ls gs://pubchem/*.sdf | sed -n '706,940p' > WorkList3
+gsutil ls gs://pubchem/*.sdf | sed -n '941,1175p' > WorkList4
+gsutil ls gs://pubchem/*.sdf | sed -n '1176,1410p' > WorkList5
+gsutil ls gs://pubchem/*.sdf | sed -n '1411,1645p' > WorkList6
+gsutil ls gs://pubchem/*.sdf | sed -n '1646,1880p' > WorkList7
+gsutil ls gs://pubchem/*.sdf | sed -n '1881,2115p' > WorkList8
+gsutil ls gs://pubchem/*.sdf | sed -n '2116,2350p' > WorkList9
+gsutil ls gs://pubchem/*.sdf | sed -n '2351,2585p' > WorkList10
+gsutil ls gs://pubchem/*.sdf | sed -n '2586,2816p' > WorkList11
 
-
+#1  236  471  706  941 1176 1411 1646 1881 2116 2351 2586
 
 
 ####
-filecontent=( `cat "PubChemWorkList"`)
+filecontent=( `cat "WorkList0"`)
 ####
 for t in "${filecontent[@]}"
 ####
