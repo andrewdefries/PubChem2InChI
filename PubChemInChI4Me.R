0dabb4612fb26c2952e17f1b49c35aae745e2aa1
andrewdefries <andrew.defries@gmail.com>
2014-05-15 05:12:14 +0000

    try on another machine batch1

--- PubChemInChI4Me.R
+++ PubChemInChI4Me.R
@@ -1,7 +1,6 @@
 ##################
 library("ChemmineR")
 ##################
-#library("RSQLite")
 
 rm(list=ls())
 p<-1
@@ -14,22 +13,33 @@ valid <- validSDF(sdfset)
 sdfset <- sdfset[valid]
 apset<-sdf2ap(sdfset)
 sdfset<-sdfset[!sapply(as(apset,"list"),length)==1]
+#cid(sdfset)<-datablocktag(sdfset, tag="PUBCHEM_COMPOUND_CID")
 ###################
 desc <- function(sdfset) {
 ###################
 ###################
-        cbind(SDFID=sdfid(sdfset),
-              datablocktag(sdfset, tag="PUBCHEM_COMPOUND_CID"),
-              datablock2ma(datablocklist=datablock(sdfset))[,12:14],
-###############
+	      cbind(SDFID=cid(sdfset),
+              #datablock2ma(datablocklist=datablock(sdfset))[,12:14],
+              ###############
               #MW=MW(sdfset),
               #groups(sdfset),
               #AP=sdf2ap(sdfset, type="character"),
               #APFP=desc2fp(x=sdf2ap(sdfset), descnames=1024, type="character")
+              PCID=datablocktag(sdfset, tag="PUBCHEM_COMPOUND_CID"),
+	      ############# 
+              IUPAC_OpenEyeName=datablocktag(sdfset, tag="PUBCHEM_IUPAC_OPENEYE_NAME"),
+              IUPAC_CAS_Name=datablocktag(sdfset, tag="PUBCHEM_IUPAC_CAS_NAME"),
+              IUPAC_Name=datablocktag(sdfset, tag="PUBCHEM_IUPAC_NAME"),
+              IUPAC_SystematicName=datablocktag(sdfset, tag="PUBCHEM_IUPAC_SYSTEMATIC_NAME"),
+              IUPAC_TraditionalName=datablocktag(sdfset, tag= "PUBCHEM_IUPAC_TRADITIONAL_NAME"),
               InChi=datablocktag(sdfset, tag="PUBCHEM_IUPAC_INCHI"),
-              InChiKey=datablocktag(sdfset, tag="PUBCHEM_IUPAC_INCHIKEY")
-##############
+              InChiKey=datablocktag(sdfset, tag="PUBCHEM_IUPAC_INCHIKEY"),
+              OpenEyeCanSmiles=datablocktag(sdfset, tag="PUBCHEM_OPENEYE_CAN_SMILES"),
+              OpenEyeISOSmiles=datablocktag(sdfset,tag="PUBCHEM_OPENEYE_ISO_SMILES"),
+              BabelSmiles=as.vector(as.character(smiset[1:length(smiset)]))##[1:length(smiset)])    
+              ##############
        )
 }
 ########################
 sdfStream(input=files[p], output=paste(gsub(".sdf", "", files[p]), ".csv", sep=""), fct=desc, Nlines=50000)
+rm(list=ls())
